711fc73a4f3fbdcb697f7dbed52194be4a23b879
Pedro Portella <pportella23@gmail.com>
2024-04-10 11:04:29 -0300

    different home card layout

--- app/page.js
+++ app/page.js
@@ -13,30 +13,20 @@ export default function Home() {
         transition={{ duration: 0.5, delay: 0.2 }}
         className="flex flex-col items-center justify-center"
       >
-        <div className="flex flex-col md:flex-row items-center justify-center">
-          <div className="md:mt-2 md:w-2/5 flex items-center justify-center">
-            <Image
-              src="/pedro.jpg"
-              alt=""
-              width={288}
-              height={288}
-              className="rounded-full shadow-2xl"
-            />
-          </div>
-          <div className="md:mt-2 md:w-1/2 md:ml-12 max-md:text-center">
-            <h1 className="text-3xl font-bold mt-6 md:mt-0 md:text-6xl">
-              Hey, I&#39;m Pedro!
-            </h1>
-            <p className="text-lg mt-4 mb-1 md:text-2xl">
-              I&#39;m a{" "}
-              <span className="font-semibold text-teal-600">
-                Software Engineer{" "}
-              </span>
-              with experience in web development, mostly{" "}
-              <span className="font-semibold text-teal-600">frontend</span>.
-            </p>
-          </div>
-        </div>
+        <Image
+          className="w-32 h-32 mb-8 rounded-full"
+          src="/pedro.jpg"
+          alt="Pedro"
+          width={128}
+          height={128}
+        />
+        <h1 className="mx-auto text-2xl font-semibold tracking-widest text-center sm:text-3xl">
+          PEDRO PORTELLA
+        </h1>
+        <hr className="w-16 my-8 border-gray-300" />
+        <h2 className="text-lg tracking-widest text-center">
+          SOFTWARE ENGINEER
+        </h2>
       </motion.div>
     </Layout>
   );
